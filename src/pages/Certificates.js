32fb1e712de72cfdef7fe2a6d3efd794639cda9e
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-10-27 15:12:18 -0300

    Redone Certificates page.

--- src/pages/Certificates.js
+++ src/pages/Certificates.js
@@ -1,6 +1,5 @@
 // Components
-import { Container, Row } from "react-bootstrap";
-import CertificateCard from "../components/CertificateCard";
+import { Container, Row, Col } from "react-bootstrap";
 // Data
 import certificates from "../data/certificates.json";
 
@@ -8,15 +7,14 @@ const Certificates = () => {
   return (
     <Container id="certificates" as="section" className="p-3">
       <Row>
-        {certificates.map(({ name, img, hours, institution, year }, idx) => (
-          <CertificateCard
-            key={idx}
-            name={name}
-            img={img}
-            hours={hours}
-            institution={institution}
-            year={year}
-          />
+        {certificates.map(({ name, img }, idx) => (
+          <Col key={idx} lg={6} className="p-2">
+            <img
+              src={img}
+              alt={name}
+              className="img-fluid"
+            />
+          </Col>
         ))}
       </Row>
     </Container>
